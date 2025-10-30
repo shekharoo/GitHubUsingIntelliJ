8ff7facc9379468f7bf6434411e6c3c06370e7fc
Shekhar Anand <shekuemail@gmail.com>
2025-10-30 14:57:36 +0530

    First commit using Intellij

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Pushing Files using Intellij using GitHub");
+        System.out.println("Pushing and commitiing Files using Intellij using GitHub");
     }
 }
